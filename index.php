ff3b077716a12d8d75cb414a4db436b69dcbf548
sophiafanjiahui <43908856+sophiafanjiahui@users.noreply.github.com>
2018-12-07 00:13:04 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+    //获得参数 signature nonce token timestamp echostr
+    $nonce     = $_GET['nonce'];
+    $token     = 'imooc';
+    $timestamp = $_GET['timestamp'];
+    $echostr   = $_GET['echostr'];
+    $signature = $_GET['signature'];
+    //形成数组，然后按字典序排序
+    $array = array();
+    $array = array($nonce, $timestamp, $token);
+    sort($array);
+    //拼接成字符串,sha1加密 ，然后与signature进行校验
+    $str = sha1( implode( $array ) );
+    if( $str == $signature && $echostr ){
+        //第一次接入weixin api接口的时候
+        echo  $echostr;
+        exit;
+    }else{
+
+        //1.获取到微信推送过来post数据（xml格式）
+        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        //2.处理消息类型，并设置回复类型和内容
+        $postObj = simplexml_load_string( $postArr );
+        //判断该数据包是否是订阅的事件推送
+        if( strtolower( $postObj->MsgType) == 'event'){
+            //如果是关注 subscribe 事件
+            if( strtolower($postObj->Event == 'subscribe') ){
+                //回复用户消息(纯文本格式)
+                $toUser   = $postObj->FromUserName;
+                $fromUser = $postObj->ToUserName;
+                $time     = time();
+                $msgType  =  'text';
+                $content  = '欢迎关注<天气小助手>公众号!您可以回复城市名称查看天气信息呦~~例如：“北京”';
+                $template = "<xml>
+                                <ToUserName><![CDATA[%s]]></ToUserName>
+                                <FromUserName><![CDATA[%s]]></FromUserName>
+                                <CreateTime>%s</CreateTime>
+                                <MsgType><![CDATA[%s]]></MsgType>
+                                <Content><![CDATA[%s]]></Content>
+                                </xml>";
+                $info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+                echo $info;
+            }
+        }else{
+        if( strtolower( $postObj->MsgType) == 'text'){
+        	$content = $postObj->Content;
+          		$toUser   = $postObj->FromUserName;
+                $fromUser = $postObj->ToUserName;
+                $time     = time();
+                $msgType  =  'text';
+                $content  = $content.'现在的天气是：晴天；温度：-3℃；   风力：西北风7米/秒；   气压：1037百帕；   空气质量：20          <更新时间>：2019年12月6日16：38';
+                $template = "<xml>
+                                <ToUserName><![CDATA[%s]]></ToUserName>
+                                <FromUserName><![CDATA[%s]]></FromUserName>
+                                <CreateTime>%s</CreateTime>
+                                <MsgType><![CDATA[%s]]></MsgType>
+                                <Content><![CDATA[%s]]></Content>
+                                </xml>";
+                $info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+                echo $info;
+        }
+        }
+
+    }
